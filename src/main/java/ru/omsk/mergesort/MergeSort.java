4df770c72b54566aa580ec874d7f56363a05feea
Кирносов Александр <serbii2010@gmail.com>
2023-01-12 21:33:54 +0600

    init commit

---
+++ src/main/java/ru/omsk/mergesort/MergeSort.java
@@ -0,0 +1,9 @@
+package ru.omsk.mergesort;
+
+import java.io.BufferedReader;
+import java.util.Iterator;
+import java.util.TreeMap;
+
+public class MergeSort<T> {
+    private TreeMap<T, BufferedReader> iterators;
+}
